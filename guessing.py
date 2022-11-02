818013a5aaa18371e9f1f11281d47bcf66e7b185
unknown <jpcashta@outlok.com>
2022-11-01 19:28:52 -0500

    initial commit

---
+++ guessing.py
@@ -0,0 +1,14 @@
+import random   
+
+guess = int(input("Pick a number between 1 and 10 peasant: "))
+rannum = random.randint(0,10)
+answer = guess
+
+while answer != rannum:
+    guess = int(input("Pick again you guessed wrong peasant: "))
+    answer = guess
+
+print("you finally got it peasant")
+
+
+
