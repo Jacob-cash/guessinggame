5e8035bda711a5de54e01b07cb5c0f5528773be4
unknown <jpcashta@outlok.com>
2022-11-01 20:20:53 -0500

    fixed try

--- guessing.py
+++ guessing.py
@@ -3,17 +3,18 @@ try:
 
     guess = int(input("Pick a number between 1 and 10 peasant: "))
     rannum = random.randint(0,10)
-    answer = guess
+    
 except ValueError:
     print("you did not choose a number , too bad.")
-    
 
-while answer != rannum:
-    
-    guess = int(input("Pick again you guessed wrong peasant: "))
+else:
     answer = guess
 
-print("you finally got it peasant")
+    while answer != rannum:
+        guess = int(input("Pick again you guessed wrong peasant: "))
+        answer = guess
+
+    print("you finally got it peasant")
 
 
 
