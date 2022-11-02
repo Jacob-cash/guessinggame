0e04293d3a3b4c09417b4abda14aeda55920be99
unknown <jpcashta@outlok.com>
2022-11-01 20:15:29 -0500

    error catch

--- guessing.py
+++ guessing.py
@@ -1,10 +1,15 @@
 import random   
+try:
 
-guess = int(input("Pick a number between 1 and 10 peasant: "))
-rannum = random.randint(0,10)
-answer = guess
+    guess = int(input("Pick a number between 1 and 10 peasant: "))
+    rannum = random.randint(0,10)
+    answer = guess
+except ValueError:
+    print("you did not choose a number , too bad.")
+    
 
 while answer != rannum:
+    
     guess = int(input("Pick again you guessed wrong peasant: "))
     answer = guess
 
